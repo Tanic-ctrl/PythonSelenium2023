0e97650b53cedb068772adfae7b3def18ada7b47
thanii <taniaradillac@gmail.com>
2023-05-03 15:11:02 -0600

    Added first practice calculator

---
+++ intro_pytest/test_cuenta_bancaria.py
@@ -0,0 +1,15 @@
+import pytest
+class CuentaBancaria:
+    def __init__(self):
+        pass
+
+    def setup_method(self):
+        pass
+
+    def teardown_method(self):
+        pass
+
+def test_cuentaBancaria():
+    pass
+
+
